af608d261f49941fb46a209f654b57769f231d96
honghyunAhn <anh2073@naver.com>
2021-08-30 19:19:07 +0900

    feat : Update board

---
+++ 210121_board/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module GTC_Study {
+	exports com.anh288.board.util;
+	exports com.anh288.staba;
+	exports com.anh288.board.Controller;
+	exports com.anh288.board;
+	exports com.anh288.board.vo;
+	exports com.anh288.board.dao;
+
+	requires mybatis;
+}
