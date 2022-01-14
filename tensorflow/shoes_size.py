3785b8ac86b739cd92d86fd37c3b5997b0ef3178
honghyunAhn <anh2073@naver.com>
2022-01-14 15:07:09 +0900

    Comment update.

--- tensorflow/shoes_size.py
+++ tensorflow/shoes_size.py
@@ -10,13 +10,13 @@ def 손실함수():
     예측값 = heigt * a + b
     return tf.square(260 - 예측값) #손실값 = (실제값 - 예측값)^2
 
-opt = tf.keras.optimizers.Adam(learning_rate=0.1) #경사하강법을 도와줌 ()를 비우면 자동으로 지정 ()안에 수동으로 지정도 가능
+opt = tf.keras.optimizers.Adam(learning_rate=0.1) #경사하강법을 이용하여 변수 값을 변경해줌 ()를 비우면 자동으로 지정 ()안에 수동으로 지정도 가능
 
 #opt.minimize(손실함수, var_list=[a,b]) #경사하강 1번 해줌 == a,b를 1번 수정해줌
 
 #반복할 수록 좋은 값이 도출된다
 for i in range(300):
-    opt.minimize(손실함수, var_list=[a,b])
+    opt.minimize(손실함수, var_list=[a,b]) #var_list는 경사하강법으로 업데이트할 weight Variable 목록
     print(a.numpy(),b.numpy()) #numpy()를 붙이면 변수에 담긴 숫자만 출력
     
 print("완료") #결과를 보면 예측한 신발사이즈 = 키 *1.52 + 1.62
