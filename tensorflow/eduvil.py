25d999ed3dcdc6e50b54631cc441499ecf932a91
honghyunAhn <anh2073@naver.com>
2022-01-18 10:47:34 +0900

    change name of variable

--- tensorflow/eduvil.py
+++ tensorflow/eduvil.py
@@ -4,18 +4,18 @@ import tensorflow as tf
 import pandas as pd
 
 data = pd.read_excel('aaa.xlsx')
-tmp = []
+tmp_lms = []
 list = []
 result = []
 
 for i, rows in data.iterrows():
     if(i > 1):
-        tmp.append([rows['Unnamed: 1'],rows['자기주도'],rows['Unnamed: 5'],
+        tmp_lms.append([rows['Unnamed: 1'],rows['자기주도'],rows['Unnamed: 5'],
                 rows['Unnamed: 6'],rows['Unnamed: 8'],rows['Unnamed: 9'],
                 rows['Unnamed: 10'],rows['Unnamed: 11'],rows['Unnamed: 12'],
                 rows['Unnamed: 13'],rows['Unnamed: 14']])
 
-for i in tmp:
+for i in tmp_lms:
     flag = True
     for j in i:
         if pd.isna(j):
