88fd8e2b93869f710c2b6305bdf2a02f1939fa22
honghyunAhn <anh2073@naver.com>
2022-01-28 00:46:13 +0900

    a coefficient of correlation

--- data_analysis/Drinks.py
+++ data_analysis/Drinks.py
@@ -10,3 +10,14 @@ print(drinks.info())
 drinks.head(10)
 
 drinks.describe()
+
+# 'beer_servings', 'wine_servings' 두 피처간의 상관계수를 계산합니다.
+# pearson은 상관계수를 구하는 계산 방법 중 하나를 의미하며, 가장 널리 쓰이는 방법입니다.
+corr = drinks[['beer_servings', 'wine_servings']].corr(method='pearson')
+print(corr)
+
+# 피처간의 상관계수 행렬을 구합니다.
+cols = ['beer_servings', 'spirit_servings',
+        'wine_servings', 'total_litres_of_pure_alcohol']
+corr = drinks[cols].corr(method='pearson')
+print(corr)
