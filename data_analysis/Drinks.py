2a8309db09f85354768e52026769d19a974a3dbe
honghyunAhn <anh2073@naver.com>
2022-02-04 23:54:52 +0900

    plt show

--- data_analysis/Drinks.py
+++ data_analysis/Drinks.py
@@ -86,3 +86,32 @@ print(continent_over_mean)
 beer_continent = drinks.groupby('continent').beer_servings.mean().idxmax()
 # idxmax() 가장 값이 큰 값을 구하겠다
 print(beer_continent)
+
+n_groups = len(result.index)
+means = result['mean'].tolist()
+mins = result['min'].tolist()
+maxs = result['max'].tolist()
+sums = result['sum'].tolist()
+
+index = np.arange(n_groups)
+bar_width = 0.1
+
+rects1 = plt.bar(index, means, bar_width,
+                 color='r',
+                 label='Mean')
+
+rects2 = plt.bar(index + bar_width, mins, bar_width,
+                 color='g',
+                 label='Min')
+
+rects3 = plt.bar(index + bar_width * 2, maxs, bar_width,
+                 color='b',
+                 label='Max')
+
+rects4 = plt.bar(index + bar_width * 3, sums, bar_width,
+                 color='y',
+                 label='Sum')
+
+plt.xticks(index, result.index.tolist())
+plt.legend()
+plt.show()
