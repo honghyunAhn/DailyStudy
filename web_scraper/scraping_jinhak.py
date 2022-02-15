bc5c2264d113cb437f6882bb06cc3d65f6724958
honghyunAhn <anh2073@naver.com>
2022-02-15 17:03:38 +0900

    Wait until the loading is complete.

--- web_scraper/scraping_jinhak.py
+++ web_scraper/scraping_jinhak.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 URL = "https://ipsi.jinhak.or.kr/subList/20000000317" # 매년마다 url을 변경해야 할 수도 있습니다.
-PATH = "/chromedriver.exe"
+PATH = "./chromedriver.exe"
 
 browser = webdriver.Chrome(PATH)
 browser.maximize_window() 
@@ -20,8 +23,15 @@ browser.find_element_by_class_name("loginPw").send_keys("Eduvil790&")
 
 # 4. 로그인 버튼 클릭
 browser.find_element_by_class_name("loginBt").click()
-time.sleep(2)
 
+# 5. 합불 사례 게시판이 나올때까지 대기
+try:
+    elem =  WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "sido_nm"))
+    )
+finally:
+    browser.quit()
+    
 select = Select(browser.find_element_by_id('sido_nm'))
 select.select_by_visible_text("경기")
 
