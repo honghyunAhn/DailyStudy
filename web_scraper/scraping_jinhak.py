202fbaccd550c5aa56beb4541fd9aaa7575d52a5
honghyunAhn <anh2073@naver.com>
2022-02-16 17:08:06 +0900

    Complete scraping jinjak

--- web_scraper/scraping_jinhak.py
+++ web_scraper/scraping_jinhak.py
@@ -4,21 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-import time
+import csv
 
 URL = "https://ipsi.jinhak.or.kr/subList/20000000317" # 매년마다 url을 변경해야 할 수도 있습니다.
 PATH = "./chromedriver.exe"
 
-browser = webdriver.Chrome(PATH)
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+browser = webdriver.Chrome(PATH, options=options)
 browser.maximize_window() 
 res = requests.get(URL)
 res.raise_for_status()
 
-soup = BeautifulSoup(res.text, "lxml")
+# 파일 형태 지정
+filename = "합불사례.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer =  csv.writer(f)
 
-# 파일 이름
-file_name = soup.find("h4", attrs={"id":"menuTitle"}).get_text()
+# 목차 생성
+title = "No.	지역	대학	지원	시기	전형유형	계열	모집단위	모집인원	최종	예비	반영영역수(탐구수)	활용지표	수능응시조합	국수탐표	국수탐백(평균)	국수탐등 국어(등급)	수학(과목명)	수학(등급)	영어(등급)	탐구(영역)	선택1(과목명)	선택1(등급)	선택2(과목명)	선택2(등급)	한국사(등급)	제2외국어(과목명)	제2외국어(등급)".split("\t")
+writer.writerow(title)
 
 # 1. 합불 사례 페이지로 이동
 browser.get(URL)
@@ -40,32 +45,42 @@ try:
 except:
     browser.quit()
 
-# 5. 지역 선택
+# 지역 선택
 sido_nm = browser.find_element_by_id("sido_nm")
 for sido in sido_nm.find_elements_by_tag_name('option'):
     sido.click()
     
-    # 6. 대학명 선택
+    # 대학명 선택
     univ_nm = browser.find_element_by_id("univ_nm")
     for univ in univ_nm.find_elements_by_tag_name('option'):
         univ.click()
         
-        # 7. 계열 선택
+        # 계열 선택
         mojip_aff = browser.find_element_by_id("mojip_aff")
         for mojip in mojip_aff.find_elements_by_tag_name('option'):
             mojip.click()
             
-            # 8. 최종합불 선택
+            # 최종합불 선택
             for rlt_final in browser.find_elements_by_name("rlt_final"):
-                rlt_final.click()
-                browser.find_element_by_id("srchbtn").click()
+                browser.execute_script("arguments[0].click();", rlt_final)
+                browser.execute_script("arguments[0].click();", browser.find_element_by_id("srchbtn"))
                 
-                # 9. 페이지 선택
+                # 페이지 선택
                 page_len = len(browser.find_elements_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[4]/ul/li/a"))
-                if page_len > 5:
+                if page_len > 4:
                     for i in range(3, page_len-1):
                         page = "/html/body/div[2]/div[2]/div[2]/div/div[4]/ul/li[{}]/a".format(i)
                         browser.find_element_by_xpath(page).click()
-                
-print("성공")                
-time.sleep(2)
+                        
+                        # 현재의 화면에서 표를 검색
+                        soup = BeautifulSoup(browser.page_source, "lxml")
+                        data_rows = soup.find("table", attrs={"class":"jstbl_list01"}).find("tbody").find_all("tr")
+                        # 1행씩
+                        for row in data_rows:
+                            columns = row.find_all("td")
+                            if len(columns) <= 1: # 의미 없는 데이터는 skip
+                                continue
+                            # 1열씩
+                            data = [column.get_text().strip() for column in columns]
+                            writer.writerow(data)
+print("저장완료")
