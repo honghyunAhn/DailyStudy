18af63c778551023947db22325e752ca2b879c2f
honghyunAhn <anh2073@naver.com>
2022-02-17 17:17:19 +0900

    end scraping jinhak

--- 06 web_scraper 2022/scraping_jinhak.py
+++ 06 web_scraper 2022/scraping_jinhak.py
@@ -1,3 +1,4 @@
+# CSV -> 엑셀 // 저장경로 지정 // 예외구문 따로 파일에 저장
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -8,7 +9,7 @@ from selenium.common.exceptions import TimeoutException
 import csv
 
 URL = "https://ipsi.jinhak.or.kr/subList/20000000317" # 매년마다 url을 변경해야 할 수도 있습니다.
-PATH = "./chromedriver.exe"
+PATH = "./chromedriver.exe" # 크롬 버전에 맞는 chromedriver가 반드시 있어야 합니다. 구글에 검색하고 다운 받는 법대로 받아서 이 파일 있는 폴더에 넣어주면 됩니다.
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -25,10 +26,15 @@ res.raise_for_status()
 filename = "합불사례.csv"
 f = open(filename, "w", encoding="utf-8-sig", newline="")
 writer =  csv.writer(f)
+fileErrname = "타임아웃.csv"
+f = open(fileErrname, "w", encoding="utf-8-sig", newline="")
+errWriter = csv.writer(f)
 
 # 목차 생성
 title = "No.	지역	대학	지원	시기	전형유형	계열	모집단위	모집인원	최종	예비	반영영역수(탐구수)	활용지표	수능응시조합	국수탐표	국수탐백(평균)	국수탐등 국어(등급)	수학(과목명)	수학(등급)	영어(등급)	탐구(영역)	선택1(과목명)	선택1(등급)	선택2(과목명)	선택2(등급)	한국사(등급)	제2외국어(과목명)	제2외국어(등급)".split("\t")
 writer.writerow(title)
+errTitle = "지역	대학	전형유형".split("\t")
+errWriter.writerow(errTitle)
 
 # 1. 합불 사례 페이지로 이동
 browser.get(URL)
@@ -53,6 +59,7 @@ except:
 # 지역 선택
 sido_nm = browser.find_element_by_id("sido_nm")
 for sido in sido_nm.find_elements_by_tag_name('option'):
+    print(sido.text, "진행중")
     sido.click()
     
     # 대학명 선택
@@ -68,14 +75,15 @@ for sido in sido_nm.find_elements_by_tag_name('option'):
             # 최종합불 선택
             for rlt_final in browser.find_elements_by_name("rlt_final"):
                 browser.execute_script("arguments[0].click();", rlt_final)
+                # 타임 아웃이 되면 타임 아웃 된 곳의 정보를 출력해 준다.
                 try: 
                     elem =  WebDriverWait(browser, 100).until(
                             EC.presence_of_element_located((By.ID, "srchbtn"))
                     )
                     browser.execute_script("arguments[0].click();", browser.find_element_by_id("srchbtn"))
                 except TimeoutException:
-                    print("지역 : ", sido.text, ", 학교: ", univ.text, ", 계열 : ", mojip.text,  " --- 타임아웃")
-                    
+                    errData = [sido.text, univ.text, mojip.text]
+                    errWriter.writerow(errData)
                 # 페이지 선택
                 page_len = len(browser.find_elements_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[4]/ul/li/a"))
                 if page_len > 4:
@@ -93,5 +101,6 @@ for sido in sido_nm.find_elements_by_tag_name('option'):
                                 continue
                             # 1열씩
                             data = [column.get_text().strip() for column in columns]
+                            # 데이터 저장
                             writer.writerow(data)
 print("저장완료")
