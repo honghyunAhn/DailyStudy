426e69813ab2485385c024d1c7b2d7308f47e795
honghyunAhn <anh2073@naver.com>
2022-02-18 17:47:11 +0900

    modify error

--- myDjango/polls/models.py
+++ myDjango/polls/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 class Choice(models.Model):
-    question = models.ForeignKey(Qusetion, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     
