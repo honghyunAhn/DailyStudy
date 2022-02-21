2331f0202eace80dea41a7b00b05c4a8e964bfbb
honghyunAhn <anh2073@naver.com>
2022-02-21 14:48:08 +0900

    modify error

--- myDjango/polls/views.py
+++ myDjango/polls/views.py
@@ -1,29 +1,30 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from urllib import response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+
+from django.shortcuts import render
 from django.urls import reverse
-from django.views import generic
+from django.shortcuts import get_object_or_404, render
 
 from .models import Choice, Qusetion
+# Create your views here.
 
+def index(request):
+    # return HttpResponse("Hello, world.")
+    latest_question_list = Qusetion.objects.order_by("-pub_date")[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+def detail(request, question_id):
+    question = get_object_or_404(Qusetion, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Qusetion.objects.order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Qusetion
-    template_name = 'polls/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Qusetion
-    template_name = 'polls/results.html'
-
+def results(request, question_id):
+    question = get_object_or_404(Qusetion, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Qusetion, pk=question_id)
