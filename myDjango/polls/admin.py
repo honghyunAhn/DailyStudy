d5a7e95ed3fe074097fdcc24b51068fc5ccd1b6f
honghyunAhn <anh2073@naver.com>
2022-02-21 11:31:28 +0900

    Use django view, import html, and connect

--- myDjango/polls/admin.py
+++ myDjango/polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Qusetion
+from .models import Choice, Qusetion
 # Register your models here.
 
 admin.site.register(Qusetion)
+admin.site.register(Choice)
