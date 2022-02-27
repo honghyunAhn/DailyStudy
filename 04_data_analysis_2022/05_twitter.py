58cd75dc8c0fea85a31904359b6571cec6b72dee
honghyunAhn <anh2073@naver.com>
2022-02-27 22:36:38 +0900

    make tweet_temp.csv

--- 04_data_analysis_2022/05_twitter.py
+++ 04_data_analysis_2022/05_twitter.py
@@ -29,3 +29,22 @@ for tweet in tweets:
     print(tweet.entities['user_mentions'])
     print(tweet.entities['hashtags'])
     print(tweet.created_at)
+
+# 크롤링된 데이터를 저장할 데이터 프레임입니다.
+columns = ['created', 'tweet_text']
+df = pd.DataFrame(columns=columns)
+
+# 크롤링을 수행할 갯수를 입력하고,Cursor 객체를 사용하여 크롤링을 수행합니다.
+max_tweets = 1000
+searched_tweets = [status for status in tweepy.Cursor(
+    api.search_tweets, q=keyword).items(max_tweets)]
+
+# '베이징'이 포함된 1000개의 트윗들에서, 'text', 'created_at' 정보를 데이터 프레임으로 저장합니다.
+for tweet in searched_tweets:
+    tweet_json = tweet._json
+    tweet_text = tweet_json['text']
+    created = tweet_json['created_at']
+    row = [created, tweet_text]
+    series = pd.Series(row, index=df.columns)
+    df = df.append(series, ignore_index=True)
+df.to_csv("tweet_temp.csv", index=False)
