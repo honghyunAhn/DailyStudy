f8dce9edfe2210410ede4265d17035b4858b7f3e
honghyunAhn <anh2073@naver.com>
2022-03-10 16:30:48 +0900

    Outputs the distribution of annual salaries.

---
+++ 04_data_analysis_2022/06_Income.py
@@ -0,0 +1,27 @@
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
+
+picher_file_path = './data/picher_stats_2017.csv'
+batter_file_path = './data/batter_stats_2017.csv'
+picher = pd.read_csv(picher_file_path)
+batter = pd.read_csv(batter_file_path)
+
+picher.columns
+
+picher.head()
+
+print(picher.shape)
+
+# 현재 OS 내에 설치된 폰트를 확인합니다.
+set(sorted([f.name for f in mpl.font_manager.fontManager.ttflist]))
+
+# 자신의 OS에 존재하는 한글 폰트를 선택합니다. 없는경우, 위의 링크에서 한글폰트 설치 후 실행합니다.
+mpl.rc('font', family='NanumGothicOTF')
+
+picher['연봉(2018)'].describe()
+picher['연봉(2018)'].hist(bins=100)  # 2018년 연봉 분포를 출력합니다.
